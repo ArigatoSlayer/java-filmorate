ebeecb892d1ba7d5d5da1e255e75f38cd57e775f
PeterD <peterdulnev@yandex.ru>
2023-11-18 02:06:44 +0300

    1 версия добавления в друзья лайки и т.д

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -59,7 +59,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/friends/common/{friendId}")
-    public List<User> mutalFriend(@PathVariable int userId, @PathVariable int friendId) {
+    public List<User> mutalFriend(@PathVariable int userId, @PathVariable int friendId){
         return service.getMutualFriends(userId, friendId);
     }
 
