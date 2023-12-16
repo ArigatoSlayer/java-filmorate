31ca9595643077ff4a717754ad2cd5691ca13085
PeterD <peterdulnev@yandex.ru>
2023-12-16 19:16:40 +0300

    databese added

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/genre")
+@RequestMapping("/genres")
 public class GenreController {
     private final GenreService service;
 
