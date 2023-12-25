81beeeaa3468e2ad046870fe1caa97a163bb665b
Sergeev-Alexander <alexander8168619@yandex.com>
2023-12-26 00:07:28 +0300

    1.0 add Director class

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Director {
+
+    private int id;
+    @NotNull
+    @NotBlank
+    private String name;
+
+}
