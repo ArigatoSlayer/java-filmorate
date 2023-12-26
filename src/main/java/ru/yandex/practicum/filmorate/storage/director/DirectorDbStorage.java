7c68d4ddac534cce836f355c05e9aee6d3b52b98
Sergeev-Alexander <alexander8168619@yandex.com>
2023-12-26 13:18:15 +0300

    1.4 fixed schema.sql

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.storage.director;
 
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import java.util.List;
 
+@Repository
 public class DirectorDbStorage implements DirectorStorage {
 
     @Override
