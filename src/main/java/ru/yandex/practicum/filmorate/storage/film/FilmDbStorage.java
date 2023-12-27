a5b240325e0e893783659daab37cb1f4fd639967
Sergeev-Alexander <alexander8168619@yandex.com>
2023-12-28 01:12:58 +0300

    1.6 removed unnecessary LinkedList init

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -151,7 +151,7 @@ public class FilmDbStorage implements FilmStorage {
                 "WHERE film_directors.director_id = ? " +
                 "GROUP BY film.film_id " +
                 "ORDER BY COUNT(likes.film_id) DESC;";
-        return new LinkedList<>(jdbcTemplate.query(sqlQuery, filmMapper, directorId));
+        return jdbcTemplate.query(sqlQuery, filmMapper, directorId);
     }
 
     @Override
@@ -161,7 +161,7 @@ public class FilmDbStorage implements FilmStorage {
                 "INNER JOIN film_directors USING (film_id) " +
                 "WHERE director_id = ? " +
                 "ORDER BY release_date;";
-        return new LinkedList<>(jdbcTemplate.query(sqlQuery, filmMapper, directorId));
+        return jdbcTemplate.query(sqlQuery, filmMapper, directorId);
     }
 
     public Set<Integer> getLikesForCurrentFilm(int filmId) {
