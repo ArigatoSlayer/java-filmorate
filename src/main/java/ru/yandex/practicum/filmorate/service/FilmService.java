bb567b27f02e045149fd6fc1d7b42003c77d0da5
Username <gregor2014@yandex.ru>
2023-12-29 13:26:05 +0300

    Add search by substring fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.storage.director.DirectorStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
