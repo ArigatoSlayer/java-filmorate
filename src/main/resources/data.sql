cc520f3f5616c208c77f053e1d6cc456068c28df
PeterD <peterdulnev@yandex.ru>
2024-01-19 19:54:46 +0300

    Исправил записи в data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
-MERGE INTO mpa_ratings (mpa_id, mpa_name) VALUES (1, 'G');
-MERGE INTO mpa_ratings (mpa_id, mpa_name) VALUES (2, 'PG');
-MERGE INTO mpa_ratings (mpa_id, mpa_name) VALUES (3, 'PG-13');
-MERGE INTO mpa_ratings (mpa_id, mpa_name) VALUES (4, 'R');
-MERGE INTO mpa_ratings (mpa_id, mpa_name) VALUES (5, 'NC-17');
+MERGE INTO mpa_ratings (mpa_id, mpa_name) VALUES (1, 'G'),
+                                                 (2, 'PG'),
+                                                 (3, 'PG-13'),
+                                                 (4, 'R'),
+                                                 (5, 'NC-17');
 
-MERGE INTO GENRE (genre_id, name) VALUES (1, 'Комедия');
-MERGE INTO GENRE (genre_id, name) VALUES (2, 'Драма');
-MERGE INTO GENRE (genre_id, name) VALUES (3, 'Мультфильм');
-MERGE INTO GENRE (genre_id, name) VALUES (4, 'Триллер');
-MERGE INTO GENRE (genre_id, name) VALUES (5, 'Документальный');
-MERGE INTO GENRE (genre_id, name) VALUES (6, 'Боевик');
+MERGE INTO GENRE (genre_id, name) VALUES (1, 'Комедия'),
+                                         (2, 'Драма'),
+                                         (3, 'Мультфильм'),
+                                         (4, 'Триллер'),
+                                         (5, 'Документальный'),
+                                         (6, 'Боевик');
 
-MERGE INTO event_type (type_id, type_name) VALUES (1, 'LIKE');
-MERGE INTO event_type (type_id, type_name) VALUES (2, 'REVIEW');
-MERGE INTO event_type (type_id, type_name) VALUES (3, 'FRIEND');
+MERGE INTO event_type (type_id, type_name) VALUES (1, 'LIKE'),
+                                                  (2, 'REVIEW'),
+                                                  (3, 'FRIEND');
 
-MERGE INTO type_operation (operation_id, operation_name) VALUES (1, 'REMOVE');
-MERGE INTO type_operation (operation_id, operation_name) VALUES (2, 'ADD');
-MERGE INTO type_operation (operation_id, operation_name) VALUES (3, 'UPDATE');
+MERGE INTO type_operation (operation_id, operation_name) VALUES (1, 'REMOVE'),
+                                                                (2, 'ADD'),
+                                                                (3, 'UPDATE');
